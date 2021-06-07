210151516f684574d0363e212c760326e1968eca
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2021-06-07 17:02:27 +0500

    Khakee/ Reset the config of prettier and eslint
    Khakee/ Reset the config of prettier and eslint

---
+++ src/HomePage/HomePage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <div className="home-page">
+      <div className="directory-menu"></div>
+    </div>
+  </>
+);
+
+export default HomePage;
