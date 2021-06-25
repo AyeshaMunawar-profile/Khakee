5f2125f60f4b49f287513f235e271b059c702ce4
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2021-06-25 13:11:50 +0500

    listen tot he state chanegs  of the user being authenticated subscribe on mount and unsubscibe on unmount

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Route, Switch} from "react-router-dom";
+import firebase from "firebase";
 import HomePage from "../Pages/homepage/homepage.component";
 import GalleryPage from "../Pages/shoppage/gallerypage.component";
 import Header from "../Components/header/header.component";
+import AuthenticationPage from "../Pages/authenticationpage/authenticationpage.component";
+import {auth} from "../Firebase/firebase.utils";
 import "../SASS/main.scss";
 import "../CSS/antd.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import AuthenticationPage from "../Pages/authenticationpage/authenticationpage.component";
 
 const App: React.FC = () => {
+	const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
+
+	let unSubscribeFromAuth: any = null;
+	useEffect(() => {
+		unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+			setCurrentUser(user);
+			// user has a value when the use is signed in even if you refresh the page firebase also implements user authenticated session persistance
+			// Firebase use O Auth services which allows use to use third party authentication services without implemeneting them manually
+			console.log("Current user is :", user);
+		});
+		return () => {
+			unSubscribeFromAuth();
+		};
+	}, []);
 	return (
 		<>
 			<Header currentPage="home" />
