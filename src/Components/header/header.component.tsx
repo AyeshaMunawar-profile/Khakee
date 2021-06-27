56e5a5017a2d4d64347f4c2b16caa2f0584ac54d
Ayesha Munawar <ayeshamunawarofficial@gmail.com>
2021-06-27 19:00:12 +0500

    khakee / Add new theme colors
    khakee / Add new theme colors

--- src/Components/header/header.component.tsx
+++ src/Components/header/header.component.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
-import {Menu, Button, Badge, Typography} from "antd";
+import {Menu, Button, Badge, Typography, Avatar, Image, Space} from "antd";
+import {UserOutlined, UserAddOutlined, ShoppingCartOutlined} from "@ant-design/icons";
 import "./header.styles.scss";
-import {UserAddOutlined, ShoppingCartOutlined} from "@ant-design/icons";
+
 import {RouteComponentProps, withRouter} from "react-router";
+import firebase from "firebase";
+import {auth} from "../../Firebase/firebase.utils";
 import SHOP_DATA from "../../Pages/shoppage/shop.data";
 
 const {Title} = Typography;
@@ -14,9 +17,10 @@ interface RouteParams {
 
 type PropsType = RouteComponentProps<RouteParams> & {
 	currentPage: string;
+	currentUser: firebase.User | null;
 };
 
-const Header: React.FC<PropsType> = ({currentPage, history}) => {
+const Header: React.FC<PropsType> = ({currentPage, currentUser, history}) => {
 	const [current, setCurrent] = useState(currentPage);
 	const handleClick = (event: any) => {
 		// console.log("click ", event);
@@ -29,7 +33,9 @@ const Header: React.FC<PropsType> = ({currentPage, history}) => {
 					<Menu.Item key="logo" className="menu-logo btn" onClick={() => history.push("/")}>
 						<img className="brand-image" src="../../Common/images/Logo/favicon/logo-192x192.png" alt="logo" />
 						<span className="brand-text">
-							<Title className="brand-name">Khakee</Title>
+							<Title className="brand-name" level={2}>
+								Khakee
+							</Title>
 							<Title level={5} className="brand-tag-line">
 								Paint yourself
 							</Title>
@@ -67,11 +73,27 @@ const Header: React.FC<PropsType> = ({currentPage, history}) => {
 					>
 						Cart
 					</Menu.Item>
-					<Menu.Item key="sign-up" className="menu-btn-sign-up btn">
-						<Button className="btn-sign-up btn-primary--pink" type="primary" shape="round" icon={<UserAddOutlined />}>
-							Sign Up
-						</Button>
-					</Menu.Item>
+
+					{/* if the current user exists then it means the user is signed in show user's profile instead of sign up button in the header  */}
+
+					{currentUser ? (
+						<Menu.Item key="user-profile" className="menu-btn-user-profile btn">
+							<Space direction="horizontal">
+								{currentUser.photoURL ? (
+									<Avatar src={currentUser.photoURL} className="user-avatar" />
+								) : (
+									<Avatar icon={<UserOutlined />} className="user-avatar" size={20} />
+								)}
+								{currentUser.displayName && currentUser.displayName}
+							</Space>
+						</Menu.Item>
+					) : (
+						<Menu.Item key="sign-up" className="menu-btn-sign-up btn">
+							<Button className="btn-sign-up btn-primary--pink" type="primary" shape="round" icon={<UserAddOutlined />}>
+								Sign Up
+							</Button>
+						</Menu.Item>
+					)}
 				</Menu>
 			</div>
 		</>
