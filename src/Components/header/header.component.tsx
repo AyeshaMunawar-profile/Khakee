45b0d911cbed55efd1fc18e471b6c85acc8790cd
Ayesha <ayeshamunawarofficial@gmail.com>
2021-06-29 23:36:14 +0500

    Add merchandise data and add it to header

--- src/Components/header/header.component.tsx
+++ src/Components/header/header.component.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {toHeaderCase} from "js-convert-case";
-import {Menu, Button, Badge, Typography, Avatar, Image, Space} from "antd";
+import {Menu, Button, Badge, Typography, Avatar, Image, Space, Row, Col, Divider} from "antd";
 import {
 	UserOutlined,
 	UserAddOutlined,
@@ -15,6 +15,7 @@ import {RouteComponentProps, withRouter} from "react-router";
 import firebase from "firebase";
 import {auth} from "../../Firebase/firebase.utils";
 import SHOP_DATA from "../../Pages/gallerypage/shop.data";
+import MERCHANDISE_DATA from "../../Pages/gallerypage/merchandise.data";
 
 const {Title} = Typography;
 
@@ -52,17 +53,42 @@ const Header: React.FC<PropsType> = ({currentPage, currentUser, history}) => {
 					<Menu.Item key="home" className="menu-home" onClick={() => history.push("/")}>
 						Home
 					</Menu.Item>
-					<SubMenu key="SubMenu" title="Products">
-						{SHOP_DATA.map((category) => (
-							<Menu.ItemGroup
-								title={toHeaderCase(category.categoryName)}
-								key={`menu-${category.categoryName}-${category.id}`}
-							>
-								{category.items.map((item) => (
-									<Menu.Item key={`sub-menu-${item.title}-${item.id}`}>{toHeaderCase(item.title)}</Menu.Item>
-								))}
-							</Menu.ItemGroup>
-						))}
+					<Menu.Item key="gallery" className="menu-gallery" onClick={() => history.push("/gallery")}>
+						Gallery
+					</Menu.Item>
+					<SubMenu key="SubMenu-Products" title="Products">
+						<Row>
+							{SHOP_DATA.map((category) => (
+								<Col>
+									<Menu.ItemGroup
+										title={toHeaderCase(category.categoryName)}
+										key={`menu-${category.categoryName}-${category.id}`}
+										className="menu-column"
+									>
+										{category.items.map((item) => (
+											<Menu.Item key={`sub-menu-${item.title}-${item.id}`}>{toHeaderCase(item.title)}</Menu.Item>
+										))}
+									</Menu.ItemGroup>
+								</Col>
+							))}
+						</Row>
+					</SubMenu>
+					<SubMenu key="SubMenu-Merchandise" title="Merchandise">
+						<Row>
+							{MERCHANDISE_DATA.map((category) => (
+								<Col>
+									<Menu.ItemGroup
+										title={toHeaderCase(category.categoryName)}
+										key={`menu-${category.categoryName}-${category.id}`}
+										className="menu-column"
+									>
+										{category.items.map((item) => (
+											<Menu.Item key={`sub-menu-${item.title}-${item.id}`}>{toHeaderCase(item.title)}</Menu.Item>
+										))}
+									</Menu.ItemGroup>
+								</Col>
+							))}
+						</Row>
 					</SubMenu>
 					<Menu.Item key="contact-me" className="menu-contact-me" onClick={() => history.push("/contact-me")}>
 						Contact Me
---
+++ src/Pages/gallerypage/merchandise.data.js
@@ -0,0 +1,219 @@
+const MERCHANDISE_DATA = [
+	{
+		categoryName: "Cups",
+		id: 1,
+		routeName: "cups",
+		items: [
+			{
+				id: 1,
+				title: "dark glory",
+				imageUrl: "../../Common/images/merch-items/cups/darkglory.jpg",
+				price: 2500
+			},
+			{
+				id: 2,
+				title: "Holy cow",
+				imageUrl: "../../Common/images/merch-items/cups/holycow.jpg",
+				price: 4500
+			},
+			{
+				id: 3,
+				title: "Snakes",
+				imageUrl: "../../Common/images/merch-items/cups/snakes.jpg",
+				price: 4500
+			},
+			{
+				id: 4,
+				title: "wisdom tooth (Brown)",
+				imageUrl: "../../Common/images/merch-items/cups/wisdomtooth.jpeg",
+				price: 2500
+			},
+			{
+				id: 5,
+				title: "wisdom tooth (Green)",
+				imageUrl: "../../Common/images/merch-items/cups/wisdomtooth green.jpeg",
+				price: 2500
+			},
+			{
+				id: 6,
+				title: "wisdom tooth (Red)",
+				imageUrl: "../../Common/images/merch-items/cups/wisdomtooth red.jpeg",
+				price: 2500
+			}
+		]
+	},
+	{
+		categoryName: "bags",
+		id: 2,
+		routeName: "bags",
+		items: [
+			{
+				id: 1,
+				title: "Abstract Art",
+				imageUrl: "../../Common/images/merch-items/bags/abstractart.jpg",
+				price: 2500
+			},
+			{
+				id: 2,
+				title: "Dreams",
+				imageUrl: "../../Common/images/merch-items/bags/dreams.jpg",
+				price: 8500
+			},
+			{
+				id: 3,
+				title: "Love Birds",
+				imageUrl: "../../Common/images/merch-items/bags/lovebirds.jpg",
+				price: 4600
+			},
+			{
+				id: 4,
+				title: "Only Friends",
+				imageUrl: "../../Common/images/merch-items/bags/onlyfriend.jpg",
+				price: 22300
+			},
+			{
+				id: 5,
+				title: "Stars",
+				imageUrl: "../../Common/images/merch-items/bags/stars.jpg",
+				price: 8900
+			},
+			{
+				id: 6,
+				title: "Swings",
+				imageUrl: "../../Common/images/merch-items/bags/swings.jpg",
+				price: 9500
+			}
+		]
+	},
+	{
+		categoryName: "Diary",
+		id: 3,
+		routeName: "diary",
+		items: [
+			{
+				id: 1,
+				title: "be Positive",
+				imageUrl: "../../Common/images/merch-items/diary/bepositive.jpeg",
+				price: 2500
+			},
+			{
+				id: 2,
+				title: "Classical Design",
+				imageUrl: "../../Common/images/merch-items/diary/classical design.jpg",
+				price: 8500
+			},
+			{
+				id: 3,
+				title: "Don't Quit",
+				imageUrl: "../../Common/images/merch-items/diary/donotstop.jpg",
+				price: 4600
+			},
+			{
+				id: 4,
+				title: "Follow your dreams",
+				imageUrl: "../../Common/images/merch-items/diary/followyourdreams.jpg",
+				price: 22300
+			},
+			{
+				id: 5,
+				title: "Stars",
+				imageUrl: "../../Common/images/merch-items/diary/bepositive.jpeg",
+				price: 8900
+			},
+			{
+				id: 6,
+				title: "Wisdom Tooth",
+				imageUrl: "../../Common/images/merch-items/diary/wisdomtooth.jpg",
+				price: 9500
+			}
+		]
+	},
+	{
+		categoryName: "Stickers",
+		id: 4,
+		routeName: "stickers",
+		items: [
+			{
+				id: 1,
+				title: "be Positive",
+				imageUrl: "../../Common/images/merch-items/diary/bepositive.jpeg",
+				price: 2500
+			},
+			{
+				id: 2,
+				title: "Skull & bones",
+				imageUrl: "../../Common/images/merch-items/diary/skull.jpg",
+				price: 8500
+			},
+			{
+				id: 3,
+				title: "Radiate Positivity",
+				imageUrl: "../../Common/images/merch-items/diary/radiatepositivity.jpg",
+				price: 4600
+			},
+			{
+				id: 4,
+				title: "I'm Happy",
+				imageUrl: "../../Common/images/merch-items/diary/iamhappy.jpg",
+				price: 22300
+			},
+			{
+				id: 5,
+				title: "Cute Faces",
+				imageUrl: "../../Common/images/merch-items/diary/cute.jpg",
+				price: 8900
+			},
+			{
+				id: 6,
+				title: "Bright Days",
+				imageUrl: "../../Common/images/merch-items/stickers/brightday.jpg",
+				price: 9500
+			}
+		]
+	},
+	{
+		categoryName: "T-Shirts",
+		id: 5,
+		routeName: "t-shirts",
+		items: [
+			{
+				id: 1,
+				title: "Childhood",
+				imageUrl: "../../Common/images/merch-items/tshirt/childhood.jpg",
+				price: 2500
+			},
+			{
+				id: 2,
+				title: "Death",
+				imageUrl: "../../Common/images/merch-items/tshirt/death.jpg",
+				price: 8500
+			},
+			{
+				id: 3,
+				title: "Drenched",
+				imageUrl: "../../Common/images/merch-items/tshirt/drenched.jpg",
+				price: 4600
+			},
+			{
+				id: 4,
+				title: "Good Morning",
+				imageUrl: "../../Common/images/merch-items/tshirt/goodmorning.png",
+				price: 22300
+			},
+			{
+				id: 5,
+				title: "Neko Revolution",
+				imageUrl: "../../Common/images/merch-items/tshirt/nekorevolution.png",
+				price: 8900
+			},
+			{
+				id: 6,
+				title: "Rabbit hole",
+				imageUrl: "../../Common/images/merch-items/tshirt/rabbithole.png",
+				price: 9500
+			}
+		]
+	}
+];
+
+export default MERCHANDISE_DATA;
